0a767dba92db0cada4b702bd082be1f61d417ebe
VasuB2001 <vabhard@hotmail.com>
2025-04-03 23:45:05 -0400

    completed task 8, updated update() function

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -5,12 +5,7 @@ import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Inventory Controller.
@@ -47,6 +42,11 @@ public class InventoryController {
   public Inventory createInventory(@RequestBody Inventory inventory) {
     return this.inventoryDAO.create(inventory);
 
+  }
+
+  @PutMapping
+  public Inventory updateInventory(@RequestBody Inventory inventory) {
+    return this.inventoryDAO.update(inventory).orElse(null);
   }
 
   /**
